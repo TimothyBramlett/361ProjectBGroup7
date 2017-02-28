1f3a050ead147a3b436d65f2aed147f4a7228fb0
timbram <timothy.bramlett@gmail.com>
2017-02-28 03:30:49 +0000

    adding more routes to app.py

--- app.py
+++ app.py
@@ -8,12 +8,20 @@ app = flask.Flask(__name__)
 # sets the 'SECRET_KEY' of the app
 app.config['SECRET_KEY'] = 'secret!'
 
-
+# default route
+# test it out by visting: https://projectbgroup7dev-timbram.c9users.io/
 # creates the default route of the web application
 @app.route('/', methods=['GET', 'POST']) # The acceptable HTTP methods for this
 def index():
     return flask.render_template('index.html')
     # renders the 'index.html' file stored in the templates directory
+
+
+# test it out by visting:  https://projectbgroup7dev-timbram.c9users.io/print_message/hello    
+@app.route('/print_message/<message>')
+def print_message(message):
+    return 'You sent the message: {}'.format(message) # only sends back raw text (no HTML)
+
 
 
 if __name__ == '__main__':
