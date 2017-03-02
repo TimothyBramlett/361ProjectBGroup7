ae3bd9c6902fa735a970fdb7ee5c8a50a6870338
timbram <timothy.bramlett@gmail.com>
2017-03-02 23:24:37 +0000

    Integrated rest_api.py with app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 # imports the flask library
 import flask
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
 
+#Create a engine for connecting to SQLite3.
+e = create_engine('sqlite:///grocery_upc.sql3')
 
 # instantiates a Flask object named app
 # passing in the special variable __name__
 app = flask.Flask(__name__)
 # sets the 'SECRET_KEY' of the app
 app.config['SECRET_KEY'] = 'secret!'
+
+api = Api(app)
 
 # default route
 # test it out by visting: https://projectbgroup7dev-timbram.c9users.io/
@@ -21,6 +28,19 @@ def index():
 @app.route('/print_message/<message>')
 def print_message(message):
     return 'You sent the message: {}'.format(message) # only sends back raw text (no HTML)
+    
+  
+  
+# https://projectbgroup7dev-timbram.c9users.io/grocery_upc_list  
+class Products(Resource):
+    def get(self):
+        #Connect to databse
+        conn = e.connect()
+        #Perform query and return JSON data
+        query = conn.execute("SELECT * FROM grocery_upc_list")
+        return {'grocery_upc_list': [i for i in query.cursor.fetchall()]}
+
+api.add_resource(Products, '/grocery_upc_list')
 
 
 
