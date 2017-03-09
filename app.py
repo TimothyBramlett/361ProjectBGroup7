4ea73b81c16110e1c86f35fb463eee203a63f9c1
timbram <timothy.bramlett@gmail.com>
2017-03-09 21:01:43 +0000

    Added lots of input validation.

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import sys
 import io
 import csv
 import sys
+import re
 
 # instantiates a Flask object named app
 # passing in the special variable __name__
@@ -106,6 +107,93 @@ def logout():
     return flask.redirect(flask.url_for('index'))
 
 #-------------------------------------------------------------------------------
+# validate zip code (returns True if input is valid, otherwise False)
+def zipIsValid(zip):
+    zipRegex = re.compile(r'\d{5}')
+    matchObj = zipRegex.search(zip)
+    if matchObj is None or len(zip) != 5:
+        return False
+    else:
+        return True
+
+#-------------------------------------------------------------------------------
+# validate state (returns True if input is valid, otherwise False)
+def stateIsValid(state):
+    states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
+              'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
+              'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
+              'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
+              'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY',
+              'DC', 'GU', 'PR', 'VI']
+    stateRegex = re.compile(r'\b(' + '|'.join(states) + r')\b')
+    matchObj = stateRegex.search(state)
+    if matchObj is None or len(state) != 2:
+        return False
+    else:
+        return True
+
+#-------------------------------------------------------------------------------
+# validate family size(returns True if input is valid, otherwise False)
+def famsizeIsValid(famsize):
+    famsizeRegex = re.compile(r'\d{1}')
+    matchObj = famsizeRegex.search(famsize)
+    if matchObj is None:
+        return False
+    try:
+        if int(famsize) < 1 or int(famsize) > 20:
+            return False
+        else:
+            return True
+    except:
+        return False
+        
+#-------------------------------------------------------------------------------
+# validate that volume is a real number greater than zero        
+# (returns True if input is valid, otherwise False)
+def volumeIsValid(volume):
+    try:
+        if float(volume) > 0.0:
+            return True
+    except:
+        pass
+    return False
+
+#-------------------------------------------------------------------------------
+# validate that quantity is an int greater than zero        
+# (returns True if input is valid, otherwise False)
+def quantityIsValid(quantity):
+    try:
+        if int(quantity) > 0:
+            return True
+    except:
+        pass
+    return False
+
+#-------------------------------------------------------------------------------
+# validate that date is valid format YYYY-MM-DD
+# simple - not getting into complex thousand year leap years, etc...
+# (returns True if input is valid, otherwise False)
+def dateIsValid(date):
+    dateRegex = re.compile(r'(\d\d\d\d)-(\d\d)-(\d\d)')
+    matchObj = dateRegex.search(date)
+    if matchObj is None or len(date) != 10:
+        return False
+    try:
+        if int(matchObj.group(1)) < 2016:
+            # no years before 2016 allowed
+            return False
+        if int(matchObj.group(2)) < 1 or int(matchObj.group(2)) > 12:
+            # bad month
+            return False
+        if int(matchObj.group(3)) < 1 or int(matchObj.group(3)) > 31:
+            # bad day of month
+            return False
+    except:
+        return False
+
+    return True
+
+#-------------------------------------------------------------------------------
 # business console route
 @app.route('/bus_console', methods=['GET', 'POST']) # The acceptable HTTP methods for this
 def bus_console():
@@ -134,6 +222,7 @@ def bus_console():
                 #---------------------------------------------------------------
                 if all (k in flask.request.form for k in ('name', 'category', 'volume', 'units', 'quantity', 'sellby', 'bestby', 'expiration')):
                     # this is a POST request from the manual input
+                    # all required fields were found
 
                     name = flask.request.form['name']
                     category = flask.request.form['category']
@@ -150,8 +239,26 @@ def bus_console():
                         cnxn.close()
                         return 'noblanksallowed', 400
                         
-                    # need to check for valid input formats
-                    
+                    if (not volumeIsValid(volume)):
+                        cnxn.close()
+                        return 'volume<=0: ' + str(row), 400
+
+                    if (not quantityIsValid(quantity)):
+                        cnxn.close()
+                        return 'non-int >0 quantity: ' + str(row), 400
+
+                    if (not dateIsValid(sellby)):
+                        cnxn.close()
+                        return 'dates should be YYYY-MM-DD: ' + str(row), 400
+
+                    if (not dateIsValid(bestby)):
+                        cnxn.close()
+                        return 'dates should be YYYY-MM-DD: ' + str(row), 400
+
+                    if (not dateIsValid(expiration)):
+                        cnxn.close()
+                        return 'dates should be YYYY-MM-DD: ' + str(row), 400
+                
                     cnxn.execute('INSERT INTO foodlosses (name, category, volume, units, quantity, sellby, bestby, expiration, bus_id) VALUES (?,?,?,?,?,?,?,?,?)',
                         (name, category, volume, units, quantity, sellby, bestby, expiration, bus_id))
                     cnxn.commit()
@@ -167,28 +274,57 @@ def bus_console():
                     inStream = io.StringIO(inFil.stream.read().decode('UTF-8'))
                     csvData = csv.DictReader(inStream)
     
-                    #for row in csvData:
-                        # check for valid rows
-                        #----------------------
-                        # correct number of fields
-                        # units contains a real number
-                        # quantity contains an integer
-                        # sellby, bestby, and expiration have right format
-                        # name, category, units not empty
-                        
-                        # if any row found to be invalid
-                        #return 'invalidfile', 415
-                        
-                        # this print statement is just for debugging
-                        #print(row['name'], row['category'], row['volume'], row['units'], row['quantity'], row['sellby'], row['bestby'], row['expiration'])
-    
+                    for row in csvData:
+                        #-------------------------------------------------------
+                        # validate in row of input data
+                        if (len(row) != 8):
+                            return 'op cancelled - no rows added - invalid record found: ' + str(row), 415
+
+                        if all (k in row for k in ('name', 'category', 'volume', 'units', 'quantity', 'sellby', 'bestby', 'expiration')):
+                            # all required fields were found
+                            pass
+                        else:
+                            return 'op cancelled - no rows added - invalid record found: ' + str(row), 415
+
+                        if (len(row['name']) == 0 or len(row['category']) == 0 or len(row['volume']) == 0 or len(row['units']) == 0):
+                            cnxn.close()
+                            return 'op cancelled - no rows added - invalid record found: ' + str(row), 415
+                            
+                        if (len(row['quantity']) == 0 or len(row['sellby']) == 0 or len(row['bestby']) == 0 or len(row['expiration']) == 0):
+                            cnxn.close()
+                            return 'op cancelled - no rows added - invalid record found: ' + str(row), 415
+
+                        if (not volumeIsValid(row['volume'])):
+                            cnxn.close()
+                            return 'op cancelled - no rows added - invalid record found - volume<=0: ' + str(row), 415
+
+                        if (not quantityIsValid(row['quantity'])):
+                            cnxn.close()
+                            return 'op cancelled - no rows added - invalid record found - non-int >0 quantity: ' + str(row), 415
+
+                        if (not dateIsValid(row['sellby'])):
+                            cnxn.close()
+                            return 'op cancelled - no rows added - invalid record found - dates should be YYYY-MM-DD: ' + str(row), 415
+
+                        if (not dateIsValid(row['bestby'])):
+                            cnxn.close()
+                            return 'op cancelled - no rows added - invalid record found - dates should be YYYY-MM-DD: ' + str(row), 415
+
+                        if (not dateIsValid(row['expiration'])):
+                            cnxn.close()
+                            return 'op cancelled - no rows added - invalid record found - dates should be YYYY-MM-DD: ' + str(row), 415
+
                     inStream.seek(0)
                     insertData = []
                     for row in csvData:
+                        #-------------------------------------------------------
+                        # build list of tuples for insertion
                         rowData = (row['name'], row['category'], row['volume'], row['units'], row['quantity'], row['sellby'], row['bestby'], row['expiration'], userid[0])
                         insertData.append(rowData)
     
                     try:
+                        #-------------------------------------------------------
+                        # insert the data into the db
                         crsr.executemany('INSERT INTO foodlosses (name, category, volume, units, quantity, sellby, bestby, expiration, bus_id) VALUES (?,?,?,?,?,?,?,?,?)', insertData)
                         cnxn.commit()
                     except sqlite3.Error as err:
@@ -219,7 +355,7 @@ def ben_console():
 @app.route('/print_message/<message>')
 def print_message(message):
     return 'You sent the message: {}'.format(message) # only sends back raw text (no HTML)
-    
+
 #-------------------------------------------------------------------------------
 # add one business to the database in table 'businesses'
 # https://projectbgroup7dev-timbram.c9users.io/bus_register
@@ -243,7 +379,10 @@ class BusRegister(Resource):
             cnxn.close()
             return 'noblanksallowed', 400
             
-        # should probably also add integer check and 5-char check for zip
+        if not zipIsValid(zip):
+            return 'invalidzip', 400
+        if not stateIsValid(state):
+            return 'invalidstate', 400
 
         user = flask.request.form['username']
         opas = flask.request.form['password']
@@ -297,13 +436,17 @@ class BenRegister(Resource):
         zip = flask.request.form['zip']
         famsize = flask.request.form['famsize']
 
-        if (len(first) == 0 or len(last) == 0 or len(addr) == 0 or len(city) == 0 or len(state) == 0 or len(zip) == 0 or famsize == 0):
+        if (len(first) == 0 or len(last) == 0 or len(addr) == 0 or len(city) == 0 or len(state) == 0 or len(zip) == 0 or len(famsize) == 0):
             cnxn.close()
             return 'noblanksallowed', 400
             
-        # should probably also add integer check and 5-char check for zip
-        # should probably also add integer check for famsize
-            
+        if not zipIsValid(zip):
+            return 'invalidzip', 400
+        if not stateIsValid(state):
+            return 'invalidstate', 400
+        if not famsizeIsValid(famsize):
+            return 'invalidfamsize', 400
+
         user = flask.request.form['username']
         opas = flask.request.form['password']
         cpas = flask.request.form['confirm_password']
